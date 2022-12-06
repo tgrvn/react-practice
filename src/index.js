96c372cc9a83d2dec135fcf0bcf5f11dc3248e1d
tgrvn <warjllalka@gmail.com>
2022-12-06 23:34:05 +0200

    fresh copy

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
+
+
+/* 
+<button onClick={handleClick}>click me</button> === 
+React.createElement('button', {onClick: handleCLick}, 'click me')
+*/
